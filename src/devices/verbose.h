258c7a683049761ae6a3ad7ff18aaca467ccbdae
Elena Marzi <marzi@student.chalmers.se>
2018-10-16 19:43:20 +0200

    Verbose output

---
+++ src/devices/verbose.h
@@ -0,0 +1,14 @@
+/*
+ * To add verbose output from the test, uncomment the following row:
+ * #define VERBOSE
+ */
+
+/* #define VERBOSE
+*/
+#ifdef VERBOSE
+#define vmsg msg
+#else
+#define vmsg (void)
+#endif
+
+
